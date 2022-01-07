a5cec1a1978ff07676dc9d16de638feacc1a44d7
pmuhire2002 <76108262+pmuhire@users.noreply.github.com>
2022-01-07 08:12:32 +0200

    Adding files for github pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import react from 'react';
 
 import { CardList } from './Components/card-list/card-list.component'
+import {SearchBox} from './Components/search-box/search-box.component'
 
 class App extends react.Component {
   constructor() {
@@ -10,22 +11,24 @@ class App extends react.Component {
       monsters: [],
       searchField: ''
     };
+    // this.handleChange=this.handleChange.bind(this); this is done to bind this to the context
   }
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(monster => this.setState({ monsters: monster }))
   }
+  handleChange=(e)=>{
+    this.setState({ searchField: e.target.value })
+  }
   render() {
     const {monsters,searchField}=this.state;
     const filteredMonsters=monsters.filter(monster=>monster.name.toLowerCase().includes(searchField.toLowerCase( )))
     return (
       <div className="App">
-        <input
-          type="search"
-          placeholder='search monsters'
-          onChange={(e) => this.setState({ searchField: e.target.value })}
-        />
+        {/* LIFTING STATE UP  */}
+        <h1>Monster Rolodex</h1>
+        <SearchBox placeholder="Search monsters" handleChange={this.handleChange}></SearchBox>
         <CardList monsters={filteredMonsters}>
         </CardList>
       </div>
